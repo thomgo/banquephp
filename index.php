7f5c7cc9b3eacf039cb20e676376b53dec159d95
Thomas <tg.59290@gmail.com>
2020-09-18 11:59:29 +0200

    display accounts on index from data function

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
-<?php include "template/nav.php"; ?>
-<?php include "template/header.php"; ?>
+<?php
+require "data/acounts.php";
+$accounts = get_accounts();
+
+include "template/nav.php";
+include "template/header.php";
+?>
 
 <h2>Vos comptes bancaires</h2>
 <div class="row mt-5">
+  <?php foreach ($accounts as $key => $account): ?>
   <div class="col-12 col-md-6 col-lg-4">
     <article class="card">
       <div class="card-header">
-        <h5 class="card-title">Compte courant</h5>
-        <h6 class="card-subtitle mb-2 text-muted">N:0132520024 fr 45</h6>
+        <h5 class="card-title"><?php echo $account["name"]; ?></h5>
+        <h6 class="card-subtitle mb-2 text-muted"><?php echo $account["number"]; ?></h6>
       </div>
       <div class="card-body">
         <ul class="list-group list-group-flush border-bottom mb-2">
-          <li class="list-group-item">Propriétaire : Mr Gossart Thomas</li>
-          <li class="list-group-item">Solde : xxxxx</li>
-          <li class="list-group-item">Dernière opération : truc -45€</li>
+          <li class="list-group-item">Propriétaire : <?php echo $account["owner"]; ?></li>
+          <li class="list-group-item">Solde : <?php echo $account["amount"]; ?></li>
+          <li class="list-group-item">Dernière opération : <?php echo $account["last_operation"]; ?></li>
         </ul>
         <a href="#" class="btn btn-info">Côturer</a>
         <a href="operation.html" class="btn btn-info">Dépot/retrait</a>
@@ -21,42 +27,8 @@
       </div>
     </article>
   </div>
-  <div class="col-12 col-md-6 col-lg-4">
-    <article class="card">
-      <div class="card-header">
-        <h5 class="card-title">Livret A</h5>
-        <h6 class="card-subtitle mb-2 text-muted">N:0132520024 fr 45</h6>
-      </div>
-      <div class="card-body">
-        <ul class="list-group list-group-flush border-bottom mb-2">
-          <li class="list-group-item">Propriétaire : Mr Gossart Thomas</li>
-          <li class="list-group-item">Solde : xxxxx</li>
-          <li class="list-group-item">Dernière opération : truc -45€</li>
-        </ul>
-        <a href="#" class="btn btn-info">Côturer</a>
-        <a href="operation.html" class="btn btn-info">Dépot/retrait</a>
-        <a href="#" class="btn btn-info">Voir</a>
-      </div>
-    </article>
-  </div>
-  <div class="col-12 col-md-6 col-lg-4">
-    <article class="card">
-      <div class="card-header">
-        <h5 class="card-title">PEL</h5>
-        <h6 class="card-subtitle mb-2 text-muted">N:0132520024 fr 45</h6>
-      </div>
-      <div class="card-body">
-        <ul class="list-group list-group-flush border-bottom mb-2">
-          <li class="list-group-item">Propriétaire : Mr Gossart Thomas</li>
-          <li class="list-group-item">Solde : xxxxx</li>
-          <li class="list-group-item">Dernière opération : truc -45€</li>
-        </ul>
-        <a href="#" class="btn btn-info">Côturer</a>
-        <a href="operation.html" class="btn btn-info">Dépot/retrait</a>
-        <a href="#" class="btn btn-info">Voir</a>
-      </div>
-    </article>
-  </div>
+<?php endforeach; ?>
+
 </div>
 
 <?php $script = "<script src='js/layer.js'></script>"; ?>
