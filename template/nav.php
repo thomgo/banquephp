f3460739e21eee496e260cbccc04a7f936bc8e4b
Thomas <tg.59290@gmail.com>
2020-10-01 16:10:12 +0200

    make public folder and change links

--- template/nav.php
+++ template/nav.php
@@ -13,7 +13,7 @@
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Alata&family=Comfortaa&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="public/css/main.css">
 
   <meta name="theme-color" content="#fafafa">
 </head>
