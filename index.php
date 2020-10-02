e0e741349c5509349aad18d4ec3a85df5cea1cea
Thomas <tg.59290@gmail.com>
2020-10-02 12:54:09 +0200

    add comments in index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require "model/accountModel.php";
-
+// Check if user is logged
 session_start();
 if(!isset($_SESSION["user"])) {
   header("Location: login.php");
 }
 
+// Get all the accounts with the last operation
 $accounts = get_accounts($db, $_SESSION["user"]);
 
 require "view/indexView.php";
